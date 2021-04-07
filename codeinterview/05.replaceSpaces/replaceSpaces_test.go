5dfeeaa15acf4cbaa4a47722a5e58f7ae3ffea44
jiangbo <jiangbo88999@gmail.com>
2021-04-07 13:33:11 +0800

    [codeinterview] add 05.replace spaces

---
+++ codeinterview/05.replaceSpaces/replaceSpaces_test.go
@@ -0,0 +1,32 @@
+package replaceSpaces
+
+import "testing"
+
+func Test_replaceSpace(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "'' => %20",
+			args: args{" "},
+			want: "%20",
+		},
+		{
+			name: "We are happy. => We%20are%20happy.",
+			args: args{"We are happy."},
+			want: "We%20are%20happy.",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := replaceSpace(tt.args.s); got != tt.want {
+				t.Errorf("replaceSpace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
