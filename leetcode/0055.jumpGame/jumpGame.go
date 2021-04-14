1ee478f051dadfdd8adc0e1649a27204425ecbed
jiangbo <jiangbo88999@gmail.com>
2021-04-14 13:47:35 +0800

    [leetcode] add 0055.JumpGame

---
+++ leetcode/0055.jumpGame/jumpGame.go
@@ -0,0 +1,41 @@
+package jumpGame
+
+// 0055. JumpGame
+// 给定一个非负整数数组nums ，你最初位于数组的 第一个下标 。
+// 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+// 判断你是否能够到达最后一个下标。
+//
+// 示例1：
+// 输入：nums = [2,3,1,1,4]
+// 输出：true
+// 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+// 示例2：
+//
+// 输入：nums = [3,2,1,0,4]
+// 输出：false
+// 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+//
+// 提示：
+// 1 <= nums.length <= 3 * 104
+// 0 <= nums[i] <= 105
+//
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/jump-game
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// canJump
+func canJump(nums []int) bool {
+	idx := 0
+	for i := 0; i < len(nums); i++ {
+		if idx < i {
+			return false
+		}
+		if i+nums[i] > idx {
+			idx = i + nums[i]
+		}
+		if idx >= len(nums)-1 {
+			return true
+		}
+	}
+	return false
+}
---
+++ leetcode/0055.jumpGame/jumpGame_test.go
@@ -0,0 +1,32 @@
+package jumpGame
+
+import "testing"
+
+func Test_canJump(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "[3,2,1,1,4] => true",
+			args: args{[]int{3, 2, 1, 1, 4}},
+			want: true,
+		},
+		{
+			name: "[3,2,1,0,4] => false",
+			args: args{[]int{3, 2, 1, 0, 4}},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canJump(tt.args.nums); got != tt.want {
+				t.Errorf("canJump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
