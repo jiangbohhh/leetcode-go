6102115df5250c67ccfdeaa356a0825462fb3fcb
jiangbo <jiangbo88999@gmail.com>
2021-04-14 18:57:11 +0800

    [leetcode] add 0240. search A 2D Matrix II

---
+++ leetcode/0240.searchA2DMatrixII/searchA2DMatrixII_test.go
@@ -0,0 +1,64 @@
+package searchA2DMatrixII
+
+import "testing"
+
+func Test_searchMatrix(t *testing.T) {
+	type args struct {
+		matrix [][]int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],5 => true",
+			args: args{
+				matrix: [][]int{
+					{1, 4, 7, 11, 15},
+					{2, 5, 8, 12, 19},
+					{3, 6, 9, 16, 22},
+					{10, 13, 14, 17, 24},
+					{18, 21, 23, 26, 30},
+				},
+				target: 5,
+			},
+			want: true,
+		},
+		{
+			name: "[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 20 => false",
+			args: args{
+				matrix: [][]int{
+					{1, 4, 7, 11, 15},
+					{2, 5, 8, 12, 19},
+					{3, 6, 9, 16, 22},
+					{10, 13, 14, 17, 24},
+					{18, 21, 23, 26, 30},
+				},
+				target: 20,
+			},
+			want: false,
+		},
+		{
+			name: "[[-5]], -5 => true",
+			args: args{
+				matrix: [][]int{
+					{-5},
+				},
+				target: -5,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchMatrix(tt.args.matrix, tt.args.target); got != tt.want {
+				t.Errorf("searchMatrix() = %v, want %v", got, tt.want)
+			}
+			if got := searchMatrix2(tt.args.matrix, tt.args.target); got != tt.want {
+				t.Errorf("searchMatrix2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
