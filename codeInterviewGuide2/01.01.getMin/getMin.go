2cc18e1f70156748ebc6da309546962aef15bd86
jiangbo <jiangbo88999@gmail.com>
2024-02-19 11:05:45 +0800

    add title description

--- codeInterviewGuide2/01.01.getMin/getMin.go
+++ codeInterviewGuide2/01.01.getMin/getMin.go
@@ -2,6 +2,12 @@ package getMin
 
 import "jiangbohhh/leetcode-go/structure/stack"
 
+// 设计一个有getMin功能的栈
+// 实现一个特殊的栈，在实现栈的基础功能上，再实现返回栈中最小元素的操作
+// 要求：
+// 1.pop、push、getMin的操作的时间复杂度都是O(1)
+// 2.设计栈可以使用现成栈结构
+
 type MinStack struct {
 	dataStack *stack.Stack
 	minStack  *stack.Stack
